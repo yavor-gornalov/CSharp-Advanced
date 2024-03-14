f8e4b118614223c4c2e4b5fd1644d2e019ba2205
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-14 20:08:35 +0200

    Reverse-a-String --uploaded

---
+++ S01_Stacks-and-Queues/P01_Reverse-a-String/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P01_Reverse_a_String
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			var stack = new Stack<char>();
+			var line = Console.ReadLine();
+
+			foreach (char ch in line)
+			{
+				stack.Push(ch);
+			}
+
+			while (stack.Count > 0)
+			{
+				Console.Write(stack.Pop());
+			};
+		}
+	}
+}

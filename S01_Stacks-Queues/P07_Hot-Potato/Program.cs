15682cc56b5f161bc11340451882665816ec69e9
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-15 22:54:55 +0200

    Hot-potato --uploaded

---
+++ S01_Stacks-Queues/P07_Hot-Potato/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P07_Hot_Potato
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var queue = new Queue<string>();
+			var children = Console.ReadLine().Split().ToArray();
+			var rotations = int.Parse(Console.ReadLine());
+
+			foreach (var child in children)
+			{
+				queue.Enqueue(child);
+			}
+
+			while (queue.Count > 1)
+			{
+				for (int i = 0; i < rotations - 1; i++)
+				{
+					queue.Enqueue(queue.Dequeue());
+				}
+				Console.WriteLine($"Removed {queue.Dequeue()}");
+			}
+			Console.WriteLine($"Last is {queue.Peek()}");
+		}
+	}
+}

f8b58d45d0a14d0028a98b317a96e3ed0d9a7757
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-15 22:54:14 +0200

    Print-Even-Numbers --uploaded

---
+++ S01_Stacks-Queues/P05_Print-Even-Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P05_Print_Even_Numbers
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			Queue<int> queue = new Queue<int>();
+
+			int[] numbers = Console.ReadLine().Split().Select(int.Parse).Where(x => x % 2 == 0).ToArray();
+
+			foreach (int number in numbers)
+			{
+				queue.Enqueue(number);
+			}
+
+			Console.WriteLine(string.Join(", ", queue.ToArray()));
+		}
+	}
+}

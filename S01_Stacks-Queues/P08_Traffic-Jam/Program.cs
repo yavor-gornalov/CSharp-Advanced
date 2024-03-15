fb127363bd245c3b598d297ad777ebf2d09e4a16
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-15 22:55:15 +0200

    Traffic-jam --uploaded

---
+++ S01_Stacks-Queues/P08_Traffic-Jam/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P08_Traffic_Jam
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			var queue = new Queue<string>();
+			int crossroadsCapacity = int.Parse(Console.ReadLine());
+			int traficCounter = 0;
+			while (true)
+			{
+				string line = Console.ReadLine();
+				if (line == "end")
+				{
+					Console.WriteLine($"{traficCounter} cars passed the crossroads.");
+					return;
+				}
+				else if (line == "green")
+				{
+					for (int i = 0; i < crossroadsCapacity; i++)
+					{
+						if (queue.Count == 0) break;
+						Console.WriteLine($"{queue.Dequeue()} passed!");
+						traficCounter++;
+					}
+				}
+				else
+				{
+					queue.Enqueue(line);
+				}
+			}
+		}
+	}
+}

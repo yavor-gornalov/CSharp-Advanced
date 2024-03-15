55992306aede369e6ea43a71963683eb1089d592
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-15 22:54:33 +0200

    Supermarket --uploaded

---
+++ S01_Stacks-Queues/P06_Supermarket/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace P06_Supermarket
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Queue<string> queue = new Queue<string>();
+			while (true)
+			{
+				string customer = Console.ReadLine();
+
+				switch (customer)
+				{
+					case "End":
+						{
+							Console.WriteLine($"{queue.Count} people remaining.");
+							return;
+						}
+					case "Paid":
+						{
+							while (queue.Count > 0)
+							{
+								Console.WriteLine(queue.Dequeue());
+							}
+							break;
+						}
+					default:
+						{
+							queue.Enqueue(customer);
+							break;
+						}
+				}
+			}
+		}
+	}
+}

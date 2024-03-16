e2a607cb99ef8c1cebc80acb5eb6ca82001414b1
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-16 09:17:20 +0200

    Fast-Food --uploaded

---
+++ S02_Stacks-Queues-Ex/P04_Fast-Food/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P04_Fast_Food
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var foodQuantity = int.Parse(Console.ReadLine());
+            var orders = new Queue<int>();
+
+            foreach (var order in Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray())
+                orders.Enqueue(order);
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count > 0)
+            {
+                if (foodQuantity < orders.Peek())
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+                    return;
+                }
+                foodQuantity -= orders.Dequeue();
+            }
+            Console.WriteLine("Orders complete");
+        }
+    }
+}

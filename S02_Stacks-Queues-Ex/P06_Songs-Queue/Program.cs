fb2af65563c6bf9aedb07fbe5d0ef1306df41be9
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-17 15:24:51 +0200

    Songs-Queue --uploaded

---
+++ S02_Stacks-Queues-Ex/P06_Songs-Queue/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace P06_Songs_Queue
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var queue = new Queue<string>();
+
+            Console.ReadLine()
+                .Split(", ")
+                .ToList()
+                .ForEach(song => queue.Enqueue(song));
+
+            while (true)
+            {
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine("No more songs!");
+                    break;
+                }
+
+                var tokens = Console.ReadLine().Split().ToArray();
+
+                var command = tokens[0];
+
+                switch (command)
+                {
+                    case "Add":
+                        {
+                            var song = string.Join(" ", tokens.Skip(1));
+                            if (queue.Contains(song))
+                            {
+                                Console.WriteLine($"{song} is already contained!");
+                                continue;
+                            }
+                            queue.Enqueue(song);
+                            break;
+                        }
+                    case "Show":
+                        {
+                            Console.WriteLine(string.Join(", ", queue));
+                            break;
+                        }
+                    case "Play":
+                        {
+                            queue.Dequeue();
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}

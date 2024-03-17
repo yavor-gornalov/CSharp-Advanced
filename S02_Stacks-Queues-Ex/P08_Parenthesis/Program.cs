89eb864b83a096722cbdce71895f041934476871
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-17 15:31:15 +0200

    Balanced-Parenthesis --uploaded

---
+++ S02_Stacks-Queues-Ex/P08_Parenthesis/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P08_Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] solutions = { "{}", "[]", "()" };
+            string expression = Console.ReadLine();
+
+            var openingBrackets = new Stack<char>();
+
+            foreach (var bracket in expression)
+            {
+                if ("{[(".Contains(bracket))
+                {
+                    openingBrackets.Push(bracket);
+                }
+                else if (")]}".Contains(bracket))
+                {
+                    if (openingBrackets.Count > 0)
+                    {
+                        var openingBracket = openingBrackets.Pop();
+                        if (solutions.Contains(openingBracket.ToString() + bracket.ToString()))
+                        {
+                            continue;
+                        }
+                    }
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+            Console.WriteLine("YES");
+        }
+    }
+}

c8a4fca41817c096f40d855840c03d185e300802
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-17 15:30:53 +0200

    Truck-Tour --uploaded

---
+++ S02_Stacks-Queues-Ex/P07_Truck-Tour/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace P07_Truck_Tour
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var pumpStations = new Queue<int[]>();
+            int numberOfPumps = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                pumpStations.Enqueue(Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray());
+            }
+
+            for (int station = 0; station < numberOfPumps; station++)
+            {
+                var currentRoute = new Queue<int[]>(pumpStations);
+
+                int currentFuel = 0;
+                int currentDistance = 0;
+                bool routeCompleted = true;
+                while (true)
+                {
+                    if (currentRoute.Count == 0) break;
+
+                    var currentPump = currentRoute.Dequeue();
+
+                    currentFuel += currentPump[0];
+                    currentDistance += currentPump[1];
+
+                    if (currentFuel < currentDistance)
+                    {
+                        routeCompleted = false;
+                        break;
+                    }
+                }
+
+                if (routeCompleted)
+                {
+                    Console.WriteLine(station);
+                    break;
+                }
+                else
+                {
+                    pumpStations.Enqueue(pumpStations.Dequeue());
+                }
+            }
+        }
+    }
+}

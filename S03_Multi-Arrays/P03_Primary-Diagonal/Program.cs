158cdfe58d5eddbbf28a1f91842c33c5df5cf942
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-18 13:34:40 +0200

    Primary-Diagonal --uploaded

---
+++ S03_Multi-Arrays/P03_Primary-Diagonal/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P03_Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int diagonalSum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                var rowElements = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                diagonalSum += rowElements[i];
+            }
+
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}

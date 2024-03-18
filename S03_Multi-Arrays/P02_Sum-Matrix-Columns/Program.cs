0981ccb325fe131f4f3537e84874e3a58bdeca42
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-18 13:34:07 +0200

    Sum-Matrix-Columns --uploaded

---
+++ S03_Multi-Arrays/P02_Sum-Matrix-Columns/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P02_Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] tokens = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            var rows = tokens[0];
+            var cols = tokens[1];
+
+            int[] colSums = new int[cols];
+            for (int i = 0; i < rows; i++)
+            {
+                var rowElements = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    colSums[j] += rowElements[j];
+                }
+            }
+
+            foreach (var sum in colSums)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}

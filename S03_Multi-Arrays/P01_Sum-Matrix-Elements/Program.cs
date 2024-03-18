991848262f5f129a2f623dcbbaff27ecd3e280d4
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-18 13:33:30 +0200

    Sum-Matrix-Elements --uploaded

---
+++ S03_Multi-Arrays/P01_Sum-Matrix-Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace P01_Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] tokens = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var rows = tokens[0];
+            var cols = tokens[1];
+
+            int sumOfElements = 0;
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] rowElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                sumOfElements += rowElements.Sum();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rowElements[j];
+                }
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine(rows.ToString());
+            sb.AppendLine(cols.ToString());
+            sb.AppendLine(sumOfElements.ToString());
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}

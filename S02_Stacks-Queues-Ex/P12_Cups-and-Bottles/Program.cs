948cd4f5fdf3b9334db2448a939ef77cd818a251
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-18 13:32:20 +0200

    Cups-and-Bottles --uploaded

---
+++ S02_Stacks-Queues-Ex/P12_Cups-and-Bottles/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace P12_Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var cups = new LinkedList<int>();
+            var bottles = new Stack<int>();
+
+            Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList()
+                .ForEach(x => cups.AddLast(x));
+
+            Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList()
+                .ForEach(x => bottles.Push(x));
+
+            int wastedWater = 0;
+            while (bottles.Count > 0 && cups.Count > 0)
+            {
+                int cupFreeCaoacity = cups.First();
+                cups.RemoveFirst();
+
+                int waterQuantity = bottles.Pop();
+
+                if (waterQuantity < cupFreeCaoacity)
+                {
+                    cups.AddFirst(cupFreeCaoacity - waterQuantity);
+                    continue;
+                }
+                wastedWater += waterQuantity - cupFreeCaoacity;
+            }
+            if (bottles.Count > 0) Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+            if (cups.Count > 0) Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}

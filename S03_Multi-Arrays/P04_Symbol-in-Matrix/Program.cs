103b7ee04666ceb20cae952bd8ad39077153b6d2
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-18 13:35:24 +0200

    Symbol-in-Matrix --uploaded

---
+++ S03_Multi-Arrays/P04_Symbol-in-Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P04_Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            string[] matrix = new string[size];
+            for (int i = 0; i < size; i++)
+            {
+                matrix[i] = Console.ReadLine();
+            }
+
+            char target = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                if (matrix[i].Contains(target))
+                {
+                    Console.WriteLine($"({i}, {matrix[i].IndexOf(target)})");
+                    return;
+                }
+            }
+            Console.WriteLine($"{target} does not occur in the matrix");
+        }
+    }
+}

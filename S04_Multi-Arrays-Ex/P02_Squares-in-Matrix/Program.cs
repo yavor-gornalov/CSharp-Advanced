df8fe852c888c2bc5453602703a0444dc7a237eb
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-19 15:18:20 +0200

    Squares-in-Matrix --uploaded

---
+++ S04_Multi-Arrays-Ex/P02_Squares-in-Matrix/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace P02_Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var tokens = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rows = tokens[0];
+            int cols = tokens[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                char[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+
+            }
+
+            int equalSqaresCount = 0;
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    if (matrix[i, j] == matrix[i, j + 1] &&
+                        matrix[i, j] == matrix[i + 1, j] &&
+                        matrix[i, j] == matrix[i + 1, j + 1])
+                    {
+                        equalSqaresCount++;
+                    }
+
+                }
+            }
+            Console.WriteLine(equalSqaresCount);
+        }
+    }
+}

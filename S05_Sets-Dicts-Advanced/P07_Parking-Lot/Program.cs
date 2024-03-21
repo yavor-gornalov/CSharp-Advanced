5da691ce993336ee89cd8a475c3d9a2163be0dca
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-21 18:26:50 +0200

    Parking-Lot --uploaded

---
+++ S05_Sets-Dicts-Advanced/P07_Parking-Lot/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace P07_Parking_Lot
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var carsInLot = new HashSet<string>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    if (carsInLot.Count == 0)
+                        Console.WriteLine("Parking Lot is Empty");
+                    else
+                        foreach (var car in carsInLot)
+                            Console.WriteLine(car);
+                    break;
+                }
+
+                var tokens = line.Split(", ").ToArray();
+
+                string command = tokens[0];
+                string licensePlate = tokens[1];
+
+                switch (command)
+                {
+                    case "IN":
+                        {
+                            carsInLot.Add(licensePlate);
+                            break;
+                        }
+                    case "OUT":
+                        {
+                            carsInLot.Remove(licensePlate);
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}

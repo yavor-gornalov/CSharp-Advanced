e47cf7e025e281274a7cf9caf48220e0e3c500f5
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-21 18:23:20 +0200

    Count-Same-Values-in-Array --uploaded

---
+++ S05_Sets-Dicts-Advanced/P01_Count-Same-Values-in-Array/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P01_Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numberOfOccurrences = new Dictionary<double, int>();
+            Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList()
+                .ForEach(x =>
+                {
+                    if (!numberOfOccurrences.ContainsKey(x))
+                        numberOfOccurrences[x] = 0;
+                    numberOfOccurrences[x]++;
+                });
+
+            foreach (var (number, occurences) in numberOfOccurrences)
+                Console.WriteLine($"{number} - {occurences} times");
+        }
+    }
+}

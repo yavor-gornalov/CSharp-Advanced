a97893eaaf5d16d8a00cecf09181c090c64be3c2
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-21 18:26:31 +0200

    Record-Unique-Names --uploaded

---
+++ S05_Sets-Dicts-Advanced/P06_Record-Unique-Names/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P06_Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var uniqueNames = new HashSet<string>();
+            int countOfNames = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfNames; i++)
+            {
+                uniqueNames.Add(Console.ReadLine());
+            }
+
+            foreach (string name in uniqueNames)
+                Console.WriteLine(name);
+        }
+    }
+}

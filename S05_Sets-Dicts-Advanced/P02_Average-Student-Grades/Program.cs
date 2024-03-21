77ce3fc633a9a838e8389e93acfe41d39fbb4ab4
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-21 18:23:40 +0200

    Average-Student-Grades --uploaded

---
+++ S05_Sets-Dicts-Advanced/P02_Average-Student-Grades/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P02_Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var studentGrades = new Dictionary<string, List<decimal>>();
+            int numberOfGrades = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfGrades; i++)
+            {
+                var line = Console.ReadLine().Split().ToArray();
+                string student = line[0];
+                decimal grade = decimal.Parse(line[1]);
+
+                if (!studentGrades.ContainsKey(student))
+                    studentGrades[student] = new List<decimal>();
+                studentGrades[student].Add(grade);
+            }
+            foreach (var (student, grades) in studentGrades)
+            {
+                Console.WriteLine($"{student} -> {string.Join(" ", grades.Select(x => x.ToString("f2")))} (avg: {grades.Average():f2})");
+            }
+        }
+    }
+}

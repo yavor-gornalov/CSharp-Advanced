b3ebb42147491d20a6e5ff340179e8e3bfd557bf
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-21 18:24:38 +0200

    Product-Shop --uploaded

---
+++ S05_Sets-Dicts-Advanced/P04_Product-Shop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace P04_Product_Shop
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var shopProducts = new Dictionary<string, Dictionary<string, double>>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "Revision")
+                {
+                    foreach (var (shopName, products) in shopProducts.OrderBy(x => x.Key))
+                    {
+                        var sb = new StringBuilder();
+                        sb.AppendLine(shopName + "->");
+                        foreach (var (productName, productPrice) in products)
+                            sb.AppendLine($"Product: {productName}, Price: {productPrice}");
+                        Console.Write(sb.ToString());
+                    }
+                    break;
+                }
+
+                var tokens = line.Split(", ").ToArray();
+                string shop = tokens[0];
+                string product = tokens[1];
+                double price = double.Parse(tokens[2]);
+
+                if (!shopProducts.ContainsKey(shop))
+                    shopProducts[shop] = new Dictionary<string, double>();
+
+                if (!shopProducts[shop].ContainsKey(product))
+                    shopProducts[shop][product] = price;
+
+            }
+        }
+    }
+}

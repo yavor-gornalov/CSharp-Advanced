58906adb68dd7ae740f6bf40fedc3ff1609dd7ab
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-21 18:24:15 +0200

    Largest-3-Numbers --uploaded

---
+++ S05_Sets-Dicts-Advanced/P03_Largest-3-Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P03_Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .Take(3)
+                .ToList()
+                .ForEach(x => Console.Write(x + " "));
+        }
+    }
+}

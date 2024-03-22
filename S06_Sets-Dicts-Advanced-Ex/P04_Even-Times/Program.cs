c89eeb33232847e83758fa0f0d252ad1329670ab
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-22 20:20:34 +0200

    Even-Times --uploaded

---
+++ S06_Sets-Dicts-Advanced-Ex/P04_Even-Times/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P04_Even_Times
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = new Dictionary<int, int>();
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(num))
+                    numbers[num] = 0;
+                numbers[num]++;
+            }
+
+            foreach (var (num, count) in numbers)
+                if (count % 2 == 0)
+                    Console.WriteLine(num);
+        }
+    }
+}

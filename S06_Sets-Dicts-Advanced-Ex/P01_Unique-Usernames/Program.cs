ddc0064f6a3c4c089ceb9281f3abf77c692f76e0
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-22 20:18:45 +0200

    Unique-Usernames --uploaded

---
+++ S06_Sets-Dicts-Advanced-Ex/P01_Unique-Usernames/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P01_Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var uniqueUsernames = new HashSet<string>();
+            int numberOfUsernames = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfUsernames; i++)
+            {
+                uniqueUsernames.Add(Console.ReadLine());
+            }
+
+            foreach (var username in uniqueUsernames)
+                Console.WriteLine(username);
+        }
+    }
+}

0677c4bedf0aa8fa7d4e388650aee151d3c8020e
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-22 20:19:20 +0200

    Sets-of-Elements --uploaded

---
+++ S06_Sets-Dicts-Advanced-Ex/P02_Sets-of-Elements/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P02_Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var tokens = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstLength = tokens[0];
+            int secondLength = tokens[1];
+
+            var firstSet = new HashSet<string>(firstLength);
+            var secondSet = new HashSet<string>(secondLength);
+
+            for (int i = 0; i < firstLength; i++)
+                firstSet.Add(Console.ReadLine());
+
+            for (int i = 0; i < secondLength; i++)
+                secondSet.Add(Console.ReadLine());
+
+            var commonElements = firstSet.Intersect(secondSet);
+
+            Console.WriteLine(string.Join(" ", commonElements));
+        }
+    }
+}

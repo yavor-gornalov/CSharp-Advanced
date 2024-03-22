36d505e556fc38f8ee8369f3fd0e688667d05c5d
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-22 20:20:06 +0200

    Periodic-Table --uploaded

---
+++ S06_Sets-Dicts-Advanced-Ex/P03_Periodic-Table/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P03_Periodic_Table
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var uniqueElements = new SortedSet<string>();
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList()
+                    .ForEach(x => uniqueElements.Add(x));
+            }
+
+            Console.WriteLine(string.Join(" ", uniqueElements));
+        }
+    }
+}

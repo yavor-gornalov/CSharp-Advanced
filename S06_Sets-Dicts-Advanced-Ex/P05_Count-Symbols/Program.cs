14a7083fc502880ccec1172c9cde006790f7d859
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-23 18:09:34 +0200

    Count-Symbols --uploaded

---
+++ S06_Sets-Dicts-Advanced-Ex/P05_Count-Symbols/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P05_Count_Symbols
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var symbolsCount = new Dictionary<char, int>();
+
+            string line = Console.ReadLine();
+
+            foreach (var symbol in line)
+            {
+                if (!symbolsCount.ContainsKey(symbol))
+                    symbolsCount[symbol] = 0;
+                symbolsCount[symbol]++;
+            }
+
+            foreach (var (symbol, count) in symbolsCount.OrderBy(x => x.Key))
+                Console.WriteLine($"{symbol}: {count} time/s");
+        }
+    }
+}

64e17306ad6be9d0fccea7e57d5285daa863b951
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-24 16:53:24 +0200

    Line-Numbers --uploaded

---
+++ S07_Steams-Files-Dirs/P02_Line-Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{++counter}. {line}");
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}

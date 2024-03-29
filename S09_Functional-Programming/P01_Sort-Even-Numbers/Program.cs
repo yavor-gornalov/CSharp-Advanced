ede84769a082f3fe4e8151f41bd44884d8e32370
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-29 18:10:07 +0200

    Sort-Even-Numbers --uploaded

---
+++ S09_Functional-Programming/P01_Sort-Even-Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P01_Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] evenNumbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .Where(x => x % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}

2b436f21de35bc536d617e50080486db8e7b0c14
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-29 18:08:21 +0200

    Even-Lines --uploaded

---
+++ S08_Steams-Files-Dirs-Ex/EvenLines/EvenLines.cs
@@ -0,0 +1,44 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            var sb = new StringBuilder();
+            var reader = new StreamReader(inputFilePath);
+
+            while (!reader.EndOfStream)
+            {
+                char[] symbolsToReplace = { '-', ',', '.', '!', '?' };
+
+                string line = reader.ReadLine();
+
+                string[] words = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Reverse()
+                    .ToArray();
+
+                string newLine = string.Join(" ", words);
+                foreach (var symbol in symbolsToReplace)
+                {
+                    while (newLine.Contains(symbol))
+                        newLine = newLine.Replace(symbol, '@');
+                }
+                sb.AppendLine(newLine);
+            }
+            return sb.ToString();
+        }
+    }
+}

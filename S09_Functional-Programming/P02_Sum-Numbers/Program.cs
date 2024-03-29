b3af58e9bd50b72c307ae2bda2991048ddef4c87
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-29 18:10:26 +0200

    Sum-Numbers --uploaded

---
+++ S09_Functional-Programming/P02_Sum-Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P02_Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Func<string, int> parser = num => int.Parse(num);
+
+            var numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToList();
+
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}

7ae4539ce0b213b66fa817ff42442ee37af303c8
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-30 15:16:37 +0200

    Knights-of-Honor --uploaded

---
+++ S10_Functional-Programming-Ex/P02_Knights-of-Honor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P02_Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Action<string> print =
+                 name => Console.WriteLine($"Sir {name}");
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(x => print(x));
+        }
+    }
+}

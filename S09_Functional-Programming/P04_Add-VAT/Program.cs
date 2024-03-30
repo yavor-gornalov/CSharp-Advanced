ec638322c7640e84f5b573160f6b97530f769196
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-30 15:14:57 +0200

    Add-VAT --uploaded

---
+++ S09_Functional-Programming/P04_Add-VAT/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P04_Add_VAT
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x * 1.20)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x.ToString("f2")));
+        }
+    }
+}

d3863e1c382f94a5da542025791f19c73179e8ea
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-30 15:14:37 +0200

    Count-Uppercase-Words --uploaded

---
+++ S09_Functional-Programming/P03_Count-Uppercase-Words/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P03_Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Predicate<string> checker = n => char.IsUpper(n[0]);
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => checker(x))
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+        }
+    }
+}

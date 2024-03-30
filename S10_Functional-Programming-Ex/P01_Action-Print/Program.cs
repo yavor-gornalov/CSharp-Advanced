dffdd36f2aaec9b3a966edfb9d18d9fb75e5dc6f
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-30 15:16:09 +0200

    Action-Print --uploaded

---
+++ S10_Functional-Programming-Ex/P01_Action-Print/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P01_Action_Print
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Action<string> print =
+                message => Console.WriteLine(message);
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(x => print(x));
+        }
+    }
+}

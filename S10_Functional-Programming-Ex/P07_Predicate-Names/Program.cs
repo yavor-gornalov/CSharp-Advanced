05284867933a2b89b187ea11587405c1992b044e
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-31 18:56:17 +0300

    Predicate-Names --uploaded

---
+++ S10_Functional-Programming-Ex/P07_Predicate-Names/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P07_Predicate_Names
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int nameMaxLength = int.Parse(Console.ReadLine());
+
+            Predicate<string> filter = x => x.Length <= nameMaxLength;
+            Action<string> print = x => Console.WriteLine(x);
+
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            names.FindAll(filter)
+                .ToList()
+                .ForEach(x => print(x));
+        }
+    }
+}

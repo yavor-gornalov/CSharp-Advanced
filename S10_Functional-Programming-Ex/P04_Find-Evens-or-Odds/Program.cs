12233cd9ec4fb61e2e8cfb4e15addf0d8134b6b2
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-31 18:55:02 +0300

    Find-Evens-or-Odds --uploaded

---
+++ S10_Functional-Programming-Ex/P04_Find-Evens-or-Odds/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P04_Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var limits = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int start = limits.Min();
+            int end = limits.Max();
+
+            string filter = Console.ReadLine();
+
+            var numbers = new List<int>();
+            for (int i = start; i <= end; i++)
+                numbers.Add(i);
+
+            Predicate<int> predicate = null;
+            if (filter == "even")
+                predicate = x => x % 2 == 0;
+
+            else if (filter == "odd")
+                predicate = x => x % 2 != 0;
+
+            Console.WriteLine(string.Join(" ", numbers.FindAll(predicate)));
+        }
+    }
+}

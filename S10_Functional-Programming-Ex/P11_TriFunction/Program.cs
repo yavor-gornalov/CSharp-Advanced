37f1839c74f58f5c265da9b1b3a30cba0a287fd7
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-31 18:58:08 +0300

    TriFunction --uploaded

---
+++ S10_Functional-Programming-Ex/P11_TriFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P11_TriFunction
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Console.WriteLine(names.First(name => name.Select(letter => (int)letter).Sum() >= N));
+
+        }
+    }
+}

d1901605de0e49c20b214afe32a60baf3ed35224
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-31 18:55:53 +0300

    Reverse-Exclude --uploaded

---
+++ S10_Functional-Programming-Ex/P06_Reverse-Exclude/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace P06_Reverse_Exclude
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            int divisor = int.Parse(Console.ReadLine());
+
+            Predicate<int> filter = (x) => x % divisor != 0;
+
+            Console.WriteLine(string.Join(" ", numbers.FindAll(filter)));
+
+        }
+    }
+}

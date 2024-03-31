2f8be889ebc7cfc971e83fb532b8789c37f43781
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-31 18:54:32 +0300

    Custom-Min --uploaded

---
+++ S10_Functional-Programming-Ex/P03_Custom-Min/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P03_Custom_Min
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Func<int[], int> customMin =
+                nums =>
+                {
+                    int min = int.MaxValue;
+                    foreach (int n in nums)
+                        if (n < min)
+                            min = n;
+                    return min;
+                };
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(customMin(numbers));
+        }
+    }
+}

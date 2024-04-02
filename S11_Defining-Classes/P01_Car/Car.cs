e061ff1bbe4b5a9edf158b0f2ee66db973b3decc
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-02 21:12:30 +0300

    Car --uploaded

---
+++ S11_Defining-Classes/P01_Car/Car.cs
@@ -0,0 +1,26 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        private string model;
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        private int year;
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+    }
+}

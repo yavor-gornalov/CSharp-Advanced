b13413ddee01f71fce80894b7c85344bae2dcb3d
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-02 21:12:52 +0300

    Car-Extension --uploaded

---
+++ S11_Defining-Classes/P02_Car-Extension/Car.cs
@@ -0,0 +1,61 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        private string model;
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        private int year;
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        private double fuelQuantity;
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+            set { this.fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return this.fuelConsumption; }
+            set { this.fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double fuelNeeded = distance * this.fuelConsumption;
+            if (this.fuelQuantity < fuelNeeded)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+                return;
+            }
+            this.fuelQuantity -= fuelNeeded;
+        }
+
+        public string WhoAmI()
+        {
+            string message = $"Make: {this.Make}\n" +
+                $"Model: {this.Model}\n" +
+                $"Year: {this.Year}\n" +
+                $"Fuel: {this.FuelQuantity:F2}";
+            return message;
+        }
+    }
+}

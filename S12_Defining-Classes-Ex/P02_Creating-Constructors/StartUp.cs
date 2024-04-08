503a8d0a4b316a707086564c36191d4e217ed56c
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-08 19:34:46 +0300

    Creating Conztructors --uploaded

---
+++ S12_Defining-Classes-Ex/P02_Creating-Constructors/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            Person secondPerson = new Person(18);
+            Person thirdPerson = new Person("Jose", 43);
+
+            Console.WriteLine($"{firstPerson.Name} - {firstPerson.Age}");
+            Console.WriteLine($"{secondPerson.Name} - {secondPerson.Age}");
+            Console.WriteLine($"{thirdPerson.Name} - {thirdPerson.Age}");
+
+        }
+    }
+}

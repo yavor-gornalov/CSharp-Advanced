ac15d518beecb3228fcc05d4c69b78a22fed58df
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-08 19:35:17 +0300

    Oldest Family Member --uploaded

---
+++ S12_Defining-Classes-Ex/P03_Oldest-Family-Member/Family.cs
@@ -0,0 +1,16 @@
+﻿namespace DefiningClasses
+{
+    internal class Family
+    {
+        List<Person> members;
+
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+
+        public void AddMember(Person member) => this.members.Add(member);
+    
+        public Person GetOldestMember() => this.members.OrderByDescending(x => x.Age).First();
+    }
+}
---
+++ S12_Defining-Classes-Ex/P03_Oldest-Family-Member/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+
+            for (int i = 0; i < N; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                var newMember = new Person(name, age);
+                family.AddMember(newMember);
+            }
+
+            var oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+
+        }
+    }
+}

6de6c71d3a0f57d1d5f3e94a257da0bae3a2d507
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-08 19:33:54 +0300

    Define-a-Class-Person --uploaded

---
+++ S12_Defining-Classes-Ex/P01_Define-a-Class-Person/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
---
+++ S12_Defining-Classes-Ex/P01_Define-a-Class-Person/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person("Peter", 20);
+            Person secondPerson = new Person("George", 18);
+            Person thirdPerson = new Person("Jose", 43);
+        }
+    }
+}

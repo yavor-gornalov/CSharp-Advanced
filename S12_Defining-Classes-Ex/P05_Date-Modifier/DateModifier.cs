172be5bc425872df4bcb70e249db6672bf087a9b
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-08 19:36:08 +0300

    Date Modifier --uploaded

---
+++ S12_Defining-Classes-Ex/P05_Date-Modifier/DateModifier.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+
+        public DateModifier(DateTime startDate, DateTime endDate)
+        {
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        public double GetDateDifference() => (this.endDate - this.startDate).TotalDays;
+
+
+    }
+}
---
+++ S12_Defining-Classes-Ex/P05_Date-Modifier/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var firstDateTokens = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var secondDateTokens = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            var firstDate = new DateTime(firstDateTokens[0], firstDateTokens[1], firstDateTokens[2]);
+            var secondDate = new DateTime(secondDateTokens[0], secondDateTokens[1], secondDateTokens[2]);
+
+            var dateModifier = new DateModifier(firstDate, secondDate);
+            var differenceInDays = Math.Abs(dateModifier.GetDateDifference());
+
+
+            Console.WriteLine(differenceInDays);
+
+
+        }
+    }
+}

90282f63ebd8174dd1c96be4e045f9d8e7f8e635
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-08 19:35:41 +0300

    Option Pool --uploaded

---
+++ S12_Defining-Classes-Ex/P04_Opinion-Poll/Family.cs
@@ -0,0 +1,21 @@
+﻿namespace DefiningClasses
+{
+    internal class Family
+    {
+        List<Person> members;
+
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+
+        public void AddMember(Person member) => this.members.Add(member);
+
+        public Person GetOldestMember() => this.members.OrderByDescending(x => x.Age).First();
+
+        public List<Person> GetMembersOverAge(int filter) => members
+            .OrderByDescending(x => x.Age)
+            .Where(x => x.Age > filter)
+            .ToList();
+    }
+}
---
+++ S12_Defining-Classes-Ex/P04_Opinion-Poll/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+
+            for (int i = 0; i < N; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                var newMember = new Person(name, age);
+                family.AddMember(newMember);
+            }
+
+            var memberOver30 = family.GetMembersOverAge(30);
+
+            foreach (var member in memberOver30.OrderBy(x => x.Name))
+                Console.WriteLine($"{member.Name} - {member.Age}");
+
+        }
+    }
+}

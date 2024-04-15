70747cd82415a78aaa85bfd2464e6b3cbd7a29fd
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-15 15:32:40 +0300

    Speed-Racing --uploaded

---
+++ S12_Defining-Classes-Ex/P06_Speed-Racing/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    internal class Car
+    {
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        private double travelledDistance;
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.travelledDistance = 0.0;
+        }
+
+        public void Drive(double amountOfKm)
+        {
+            double fuelNeeded = amountOfKm * this.FuelConsumptionPerKilometer;
+            if (this.FuelAmount < fuelNeeded)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+                return;
+            }
+            this.travelledDistance += amountOfKm;
+            this.FuelAmount -= fuelNeeded;
+        }
+
+        public override string ToString() 
+            => $"{this.Model} {this.FuelAmount:F2} {this.travelledDistance}";
+    }
+}
---
+++ S12_Defining-Classes-Ex/P06_Speed-Racing/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace SpeedRacing
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            var cars = new List<Car>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var model = tokens[0];
+                var fuelAmount = double.Parse(tokens[1]);
+                var fuelConsumptionPerKilometer = double.Parse(tokens[2]);
+
+                var car = new Car(model, fuelAmount, fuelConsumptionPerKilometer);
+
+                cars.Add(car);
+            }
+
+            string line;
+            while ((line = Console.ReadLine()) != "End")
+            {
+                var tokens = line
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Skip(1)
+                    .ToArray();
+
+                var carModel = tokens[0];
+                var amountOfKm = double.Parse(tokens[1]);
+
+                var car = cars.FirstOrDefault(x => x.Model == carModel);
+                car.Drive(amountOfKm);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}

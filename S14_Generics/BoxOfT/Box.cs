c1798d2d48163b3b2c35e56100255adcbe843e52
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-16 09:59:50 +0300

    Box-Of-T --uploaded

---
+++ S14_Generics/BoxOfT/Box.cs
@@ -0,0 +1,32 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public T Remove()
+        {
+
+            T removedItem = this.data[this.data.Count - 1];
+            this.data.RemoveAt(this.data.Count - 1);
+            return removedItem;
+        }
+        public int Count
+        {
+            get
+            {
+                return this.data.Count;
+            }
+        }
+    }
+}

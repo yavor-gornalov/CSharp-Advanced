44a527de83351a4d46b8ca482a543ebdeb3c14c9
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-16 10:01:15 +0300

    Generic-Array-Creator --uploaded

---
+++ S14_Generics/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericArrayCreator
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+            strings.ToList().ForEach(s => { Console.WriteLine(s); });
+        }
+
+    }
+}

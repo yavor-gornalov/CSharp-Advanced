8e8201cdee3143d31e56bd3ee71605878110d4b2
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-16 10:02:04 +0300

    Generic-Scale --uploaded

---
+++ S14_Generics/GenericScale/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericScale
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var equalScale = new EqualityScale<int>(2, 5);
+            Console.WriteLine(equalScale.AreEqual());
+        }
+    }
+}

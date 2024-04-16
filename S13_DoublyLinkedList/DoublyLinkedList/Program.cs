c29f8f277c537f00b6d7b44eca7fad2d15f72ffe
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-16 09:57:53 +0300

    DoublyLinkedList

---
+++ S13_DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DoublyLinkedList
+{
+    internal class Test
+    {
+        static void Main()
+        {
+            var dll = new DoublyLinkedList<int>();
+
+            dll.AddFirst(1);
+            dll.AddFirst(5);
+            dll.AddLast(3);
+            dll.AddLast(4);
+            dll.AddLast(5);
+
+            dll.RemoveLast();
+            dll.RemoveFirst();
+
+            dll.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine($"DoublyLinkedList Count: {dll.Count}");
+
+            int[] arr = dll.ToArray();
+            Console.WriteLine(arr[1]);
+        }
+    }
+}

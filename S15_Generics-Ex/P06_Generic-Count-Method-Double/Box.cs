45b463bdaba5be4f906290cbd998fe7c80f2d13c
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-17 16:09:15 +0300

    Generic-Count-Method-Double --uploaded

---
+++ S15_Generics-Ex/P06_Generic-Count-Method-Double/Box.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace GenericCountMethodStringDouble
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private readonly List<T> list;
+
+        public Box()
+        {
+            this.list = new List<T>();
+        }
+
+        public void Add(T item) { 
+            this.list.Add(item); 
+        }
+        
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            this.list.ForEach(el => sb.AppendLine($"{typeof(T)}: {el}"));
+            return sb.ToString();
+        }
+
+        public int CountOfGreaterElements (T item) {
+            return this.list.Where(el => el.CompareTo(item) > 0).Count();
+        }
+
+    }
+}
---
+++ S15_Generics-Ex/P06_Generic-Count-Method-Double/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericCountMethodStringDouble
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double element = double.Parse(Console.ReadLine());
+                box.Add(element);
+            }
+
+            var item = double.Parse( Console.ReadLine());
+            
+            Console.WriteLine(box.CountOfGreaterElements(item));
+        }
+
+
+    }
+}

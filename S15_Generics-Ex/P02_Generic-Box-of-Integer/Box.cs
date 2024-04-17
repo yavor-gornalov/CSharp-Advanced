47b7205d47aff949a35450d8b99a26b1e460ff31
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-17 16:06:20 +0300

    Generic-Box-of-Integer --uploaded

---
+++ S15_Generics-Ex/P02_Generic-Box-of-Integer/Box.cs
@@ -0,0 +1,18 @@
+﻿namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private readonly T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        //public override string ToString() => this.value.ToString();
+        public override string ToString()
+        {
+            return $"{this.value.GetType().ToString()}: {this.value}";
+        }
+    }
+}
---
+++ S15_Generics-Ex/P02_Generic-Box-of-Integer/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfInteger
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                var box = new Box<int>(value);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}

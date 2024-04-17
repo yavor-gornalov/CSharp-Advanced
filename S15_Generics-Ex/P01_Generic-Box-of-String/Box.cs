f9662220151106063a22596a5f4b075d0b4a7357
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-17 16:05:42 +0300

    Generic-Box-Of-String  --uploaded

---
+++ S15_Generics-Ex/P01_Generic-Box-of-String/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private readonly T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        //public override string ToString() => this.value.ToString();
+        public override string ToString()
+        {
+            return $"{this.value.GetType().ToString()}: {this.value}";
+        }
+    }
+}
---
+++ S15_Generics-Ex/P01_Generic-Box-of-String/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfString
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string value = Console.ReadLine();
+                var box = new Box<string>(value);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}

f24b3b046bc6b2b29d307f628b3a5bed48f53e65
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-17 16:08:28 +0300

    Generic-Count-Method-String --uploaded

---
+++ S15_Generics-Ex/P05_Generic-Count-Method-String/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericCountMethodString
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string element = Console.ReadLine();
+                box.Add(element);
+            }
+
+            var item = Console.ReadLine();
+            
+            Console.WriteLine(box.CountOfGreaterElements(item));
+        }
+    }
+}

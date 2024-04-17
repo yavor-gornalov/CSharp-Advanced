204dcded6a32ffc4f4fc8445c28048d324e36d6a
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-17 16:07:38 +0300

    Generic-Swap-Method-Integer --uploaded

---
+++ S15_Generics-Ex/P04_Generic-Swap-Method-Integer/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        private readonly T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.value}";
+        }
+    }
+}
---
+++ S15_Generics-Ex/P04_Generic-Swap-Method-Integer/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace GenericSwapMethodInteger
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var listOfBoxes = new List<Box<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                var box = new Box<int>(value);
+                listOfBoxes.Add(box);
+            }
+        
+            var tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int first = tokens[0];
+            int second = tokens[1];
+
+            swapCommand(listOfBoxes, first, second);
+
+            listOfBoxes.ForEach(b => Console.WriteLine(b));
+        }
+
+        public static void swapCommand<T>(List<T> arr, int firstIdx, int secondIdx) { 
+            var temp = arr[firstIdx];
+            arr[firstIdx] = arr[secondIdx];
+            arr[secondIdx] = temp;
+        }
+    }
+}

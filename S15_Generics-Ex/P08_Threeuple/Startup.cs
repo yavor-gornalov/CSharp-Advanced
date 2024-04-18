00a6beb76209305e48b438d8fd8b5ed873ffe17d
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-18 11:14:24 +0300

    Threeuple --uploaded

---
+++ S15_Generics-Ex/P08_Threeuple/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace Threeuple
+{
+    internal class Startup
+    {
+        static void Main()
+        {
+            var personTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var person = new Threeuple<string, string, string>($"{personTokens[0]} {personTokens[1]}", personTokens[2], String.Join(" ", personTokens.Skip(3).ToList()));
+            Console.WriteLine(person);
+
+            var beerTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var beer = new Threeuple<string, int, bool>(beerTokens[0], int.Parse(beerTokens[1]), beerTokens[2] == "drunk");
+            Console.WriteLine(beer);
+
+            var accountTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var account = new Threeuple<string, double, string>(accountTokens[0], double.Parse(accountTokens[1]), accountTokens[2]);
+            Console.WriteLine(account);
+        }
+    }
+}
---
+++ S15_Generics-Ex/P08_Threeuple/Threeuple.cs
@@ -0,0 +1,16 @@
+﻿namespace Threeuple
+{
+    public class Threeuple<T1, T2, T3> : CustomTuple<T1, T2>
+    {
+        private readonly T3 item3;
+
+        public Threeuple(T1 item1, T2 item2, T3 item3) : base(item1, item2)
+        {
+            this.item3 = item3;
+        }
+
+        public T3 Item3 { get { return this.item3; } }
+
+        public override string ToString() => $"{Item1} -> {Item2} -> {Item3}";
+    }
+}

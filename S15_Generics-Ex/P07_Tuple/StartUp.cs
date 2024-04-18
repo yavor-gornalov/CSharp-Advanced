29eac220265e5ab5f94a7c7c9119bff079042341
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-18 11:13:58 +0300

    Custom-Tupple --uploaded

---
+++ S15_Generics-Ex/P07_Tuple/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomTuple
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var personTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var person = new CustomTuple<string, string>($"{personTokens[0]} {personTokens[1]}", personTokens[2]);
+            Console.WriteLine(person);
+
+            var beerTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var beer = new CustomTuple<string, int>(beerTokens[0], int.Parse(beerTokens[1]));
+            Console.WriteLine(beer);
+
+            var numbersTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var numbers = new CustomTuple<int, double>(int.Parse(numbersTokens[0]), double.Parse(numbersTokens[1]));
+            Console.WriteLine(numbers);
+        }
+    }
+}
---
+++ S15_Generics-Ex/P08_Threeuple/CustomTuple.cs
@@ -0,0 +1,19 @@
+﻿namespace Threeuple
+{
+    public class CustomTuple<T1, T2>
+    {
+        private readonly T1 item1;
+        private readonly T2 item2;
+
+        public CustomTuple(T1 item1, T2 item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public T1 Item1 { get{ return this.item1; } }
+        public T2 Item2 { get { return this.item2; } }
+
+        public override string ToString() => $"{Item1} -> {Item2}";
+    }
+}

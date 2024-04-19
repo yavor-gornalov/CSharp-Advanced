75f2649133106ecbd74a10151e1477fddcfbb036
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-19 15:40:30 +0300

    Collection --uploaded

---
+++ S17_SIterators-and-Comparators-Ex/P02_Collection/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace Collection
+{
+    public class ListyIterator<T>:IEnumerable<T>
+    {
+        private List<T> _list;
+        private int _index = 0;
+
+        public ListyIterator(List<T> list)
+        {
+            this._list = list;
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this._index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext() => this._index < this._list.Count - 1;
+
+        public void Print() {
+            if (this._list.Count == 0)
+                throw new InvalidOperationException();
+            Console.WriteLine(this._list[this._index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this._list.Count; i++)
+                yield return this._list[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ S17_SIterators-and-Comparators-Ex/P02_Collection/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace Collection
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var tokens = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();
+
+            var listy = new ListyIterator<string>(tokens);
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "Move":
+                        {
+                            Console.WriteLine(listy.Move());
+                            break;
+                        }
+                    case "HasNext":
+                        {
+                            Console.WriteLine(listy.HasNext());
+                            break;
+                        }
+                    case "Print":
+                        {
+                            try
+                            {
+                                listy.Print();
+                            } catch (InvalidOperationException) {
+                                Console.WriteLine("Invalid Operation!");
+                            }
+                            break;
+                        }
+                    case "PrintAll":
+                        {
+                            Console.WriteLine(String.Join(" ", listy));
+                            break;
+                        }
+                    case "END": return;
+                }
+            }
+        }
+    }
+}

70acd8e26c68b4d8754bb3326ba875d737d6e61f
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-19 15:40:58 +0300

    Stack --uploaded

---
+++ S17_SIterators-and-Comparators-Ex/P03_Stack/CustomStack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace Stack
+{
+    internal class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> _stack;
+
+        public CustomStack()
+        {
+            _stack = new List<T>();
+        }
+
+        public void Push(T[] items)
+        {
+            foreach (var item in items)
+            {
+                this._stack.Add(item);
+            }
+        }
+        public T Pop()
+        {
+            if (this._stack.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            int lastIdx = this._stack.Count - 1;
+            var item = this._stack[lastIdx];
+            this._stack.RemoveAt(lastIdx);
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = this._stack.Count - 1; i >= 0; i--)
+                yield return this._stack[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ S17_SIterators-and-Comparators-Ex/P03_Stack/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace Stack
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var myStack = new CustomStack<string>();
+            var delims = new char[] { ',', ' ' };
+
+
+            string line;
+            while ((line = Console.ReadLine()) != "END")
+            {
+                if (line.StartsWith("Pop"))
+                {
+                    try
+                    {
+                        myStack.Pop();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                }
+                else if (line.StartsWith("Push"))
+                {
+                    var elements = line
+                        .Split(delims, StringSplitOptions.RemoveEmptyEntries)
+                        .Skip(1)
+                        .ToArray();
+                    myStack.Push(elements);
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(String.Join("\n", myStack));
+            }
+        }
+    }
+}

a114621b48fc7ae7a711a739772a4693fb757796
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-19 15:37:55 +0300

    Library --uploaded

---
+++ S16_SIterators-and-Comparators/P01_Library/Library.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books.OrderBy(b => b, new BookComparator()).ToList());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+                this.currentIndex = -1;
+            }
+
+            public Book Current => this.books[currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()=> ++this.currentIndex < books.Count;
+
+            public void Reset() => this.currentIndex = -1;
+        }
+    }
+}

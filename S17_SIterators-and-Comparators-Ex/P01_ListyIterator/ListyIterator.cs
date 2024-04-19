6c0c83790d982bf8e7433e1bc592477ee364dd1c
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-19 15:40:05 +0300

    BookComparator --uploaded

---
+++ S17_SIterators-and-Comparators-Ex/P01_ListyIterator/ListyIterator.cs
@@ -0,0 +1,31 @@
+﻿namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> _list;
+        private int _index = 0;
+
+        public ListyIterator(List<T> list)
+        {
+            this._list = list;
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this._index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext() => this._index < this._list.Count - 1;
+
+        public void Print() {
+            if (this._list.Count == 0)
+                throw new InvalidOperationException();
+            Console.WriteLine(this._list[this._index]);
+        }
+    }
+}

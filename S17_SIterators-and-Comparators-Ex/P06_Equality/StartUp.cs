84dd986c67b3bdcbd7ff78021ee448e2bef69497
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-20 12:38:48 +0300

    Custom-Comparator --uploaded

---
+++ S17_SIterators-and-Comparators-Ex/P06_Equality/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace EqualityLogic
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var sortedSet = new SortedSet<Person>();
+            var hashSet = new HashSet<Person>();
+
+            int numberOfpeople = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfpeople; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                var person = new Person(name: tokens[0], age: int.Parse(tokens[1]));
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
---
+++ S17_SIterators-and-Comparators-Ex/P07_Comparator/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace CustomComparator
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(arr, CustomComparator);
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+
+        public static int CustomComparator(int x, int y)
+        {
+            if (isEven(x) && isEven(y) || !isEven(x) && !isEven(y))
+                return x.CompareTo(y);
+            if (isEven(x))
+                return -1;
+            return 1;
+        }
+
+        private static bool isEven(int x) => x % 2 == 0;
+    }
+}

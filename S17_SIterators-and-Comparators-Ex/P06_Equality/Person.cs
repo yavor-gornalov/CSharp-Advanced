ea1880bd21b70267879a15bca2f5284e6a1649fc
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-20 12:38:10 +0300

    Equality-Logic --uploaded

---
+++ S17_SIterators-and-Comparators-Ex/P06_Equality/Person.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EqualityLogic
+{
+    internal class Person : IComparable<Person>
+    {
+        private readonly string _name;
+        private readonly int _age;
+
+        public Person(string name, int age)
+        {
+            this._name = name;
+            this._age = age;
+        }
+
+        public string Name
+        {
+            get => this._name;
+        }
+
+        public int Age
+        {
+            get => this._age;
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result != 0)
+                return result;
+            return this.Age.CompareTo(other.Age);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() ^ this.Age.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person other = obj as Person;
+            if (other == null)
+                return false;
+            return this.Name == other.Name && this.Age == other.Age;
+        }
+    }
+}

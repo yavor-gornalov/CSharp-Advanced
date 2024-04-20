df8ad0c14632942231299e3e761400e77c4fc438
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-20 12:37:23 +0300

    Froggy --uploaded

---
+++ S17_SIterators-and-Comparators-Ex/P04_Froggy/Lake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace Froggy
+{
+    internal class Lake : IEnumerable<int>
+    {
+        private List<int> _stones;
+        public Lake(List<int> stones)
+        {
+            this._stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this._stones.Count; i += 2)
+                yield return this._stones[i];
+
+            for (int i = this._stones.Count - 1; i >= 0; i--)
+                if (i % 2 != 0)
+                    yield return this._stones[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ S17_SIterators-and-Comparators-Ex/P04_Froggy/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using Froggy;
+
+namespace Froggy
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var lake = new Lake(stones);
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}

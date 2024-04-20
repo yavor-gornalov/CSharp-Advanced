fa4fcb2b1b0d9068c049bae9e9eaf390927cff0a
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-20 12:37:48 +0300

    Comparing-Objects --uploaded

---
+++ S17_SIterators-and-Comparators-Ex/P05_Comparing/Person.cs
@@ -0,0 +1,46 @@
+﻿namespace ComparingObjects
+{
+    internal class Person : IComparable<Person>
+    {
+        private readonly string _name;
+        private readonly int _age;
+        private readonly string _town;
+
+        public Person(string name, int age, string town)
+        {
+            this._name = name;
+            this._age = age;
+            this._town = town;
+        }
+
+        public string Name
+        {
+            get => this._name;
+        }
+
+        public int Age
+        {
+            get => this._age;
+        }
+
+        public string Town
+        {
+            get => this._town;
+        }
+
+        public int CompareTo(Person other)
+        {
+            {
+                if (other == null) return 1;
+
+                int nameComarison = this.Name.CompareTo(other.Name);
+                if (nameComarison != 0) return nameComarison;
+
+                int ageComparison = this.Age.CompareTo(other.Age);
+                if (ageComparison != 0) return ageComparison;
+
+                return this.Town.CompareTo(other.Town);
+            }
+        }
+    }
+}
---
+++ S17_SIterators-and-Comparators-Ex/P05_Comparing/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace ComparingObjects
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var people = new List<Person>();
+            string line;
+            while ((line = Console.ReadLine()) != "END")
+            {
+                var personInfo = line
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var person = new Person(
+                        name: personInfo[0],
+                        age: int.Parse(personInfo[1]),
+                        town: personInfo[2]
+                    );
+
+                people.Add(person);
+            }
+
+            int personNumber = int.Parse(Console.ReadLine()) - 1;
+            int totalPeople = people.Count();
+            var tartgetPerson = people[personNumber];
+            int countOfMatches = people.Where(p => p.CompareTo(tartgetPerson) == 0).Count();
+
+            if (countOfMatches == 1)
+                Console.WriteLine("No matches");
+            else
+                Console.WriteLine($"{countOfMatches} {totalPeople - countOfMatches} {totalPeople}");
+        }
+    }
+}
